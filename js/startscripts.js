9e5a7d390510c175351915d639ef2a420d1e01c1
xdpirate <1757462+xdpirate@users.noreply.github.com>
2024-06-13 00:38:43 +0200

    Remove 'saved' arg so we don't show false notifs. on reload

--- js/startscripts.js
+++ js/startscripts.js
@@ -236,6 +236,7 @@ function notifyDBChange() {
     }
     
     showToastNotification(`✓ ${component} ${action}!`);
+    history.replaceState({}, document.title, `?t=${params.get("t")}`);
 }
 
 function showToastNotification(text) {
